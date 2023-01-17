8f2ba8ec95ea97abc4184d0a612315db6a171453
esoteric918 <3411@holbertonstudent.com>
2023-01-16 22:04:37 -0600

    +filterdrawer

--- ui-components/navBar.tsx
+++ ui-components/navBar.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect, useContext } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket,  faBars, faShirt, faPeopleRobbery } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import ProfileCard from './ProfileCard';
 import { Auth } from '@aws-amplify/auth'
 import AppContext, { AppInfo } from '../contexts/appContext'
 import { useRouter } from 'next/router';
 import AuthContext from "../contexts/authContext"
-import GarMint from '../models/garmint'
+import GarMint from '../models/garmint';
+import GarmintCam  from './GarmintCam';
 
 const appInfo = new (AppInfo)
 
@@ -59,17 +61,24 @@ const Navbar = () => {
 				<div className='item-center pr-3'>
 					<Image src={'/assets/profile_HST.jpg'} alt="Tofer" width={75} height={65} className='rounded-full mt-5' />
 				</div>
-				<div className='flex flex-col w-full  text-white'>
-					<p className='capitalize font-bold mt-4 mb-2'>{user.attributes.preferred_username}</p>
-					<FontAwesomeIcon icon={faShirt} className='text-lg text-green-400' />{seasonTops.length}
+				<div className='grid grid-rows-3 grid-flow-col gap-4 w-full text-white'>
+					<p className='capitalize font-bold mt-4 mb-2 text-amber-900 '>{user.attributes.preferred_username}</p>
+					<FontAwesomeIcon icon={fas.faShirt} className='text-lg text-green-400'></FontAwesomeIcon>
+                    {seasonTops.length}
+                    <Image src={'/assets/icons/pants_icon.webp'} alt='icon_pants' width={30} height={30} className='fill-green-300 dark:fill-green-400'></Image>
                     {seasonBottoms.length}
-					<FontAwesomeIcon icon={faPeopleRobbery} className='text-lg text-green-400' />
-
-
-					<p>Wash Percent {washPercent}</p>
+					<p>
+                        <FontAwesomeIcon icon={fas.faFoucet} className='text-lg text-amber-900'></FontAwesomeIcon>
+                        {washPercent}
+                    </p>
 
 				</div>
-				<div className='hidden md:flex sm:justify-end  text-white'>
+
+                <button
+                onClick={() => {}}>
+                    <FontAwesomeIcon icon={fas.faCameraRetro} className='text-lg text-amber-900 pr-6'></FontAwesomeIcon>
+                </button>
+				<div className='hidden md:flex left-0 top-0 z-10 text-white'>
 
 					<button onClick={async () => {
 						await Auth.signOut()
@@ -77,14 +86,14 @@ const Navbar = () => {
 						setAppContext(new AppInfo())
 						router.push('/')
 
-					}}><FontAwesomeIcon icon={faRightFromBracket}></FontAwesomeIcon></button>
+					}}><FontAwesomeIcon icon={fas.faRightFromBracket}></FontAwesomeIcon></button>
 				</div>
 				{/* Hamburger Icon */}
 				<div
 					onClick={handleNav}
 					className='md:hidden'
 				>
-					<FontAwesomeIcon icon={faBars} className='ml-10 text-sm' />
+					<FontAwesomeIcon icon={fas.faBars} className='ml-10 text-sm' />
 				</div>
 
 			</div>
